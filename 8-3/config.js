8ebb07f4488ce48f7f0be9032c5016261382901b
DinoTranVN <107344217+DinoTranVN@users.noreply.github.com>
2023-03-06 10:32:29 +0700

    Add files via upload

---
+++ 8-3/config.js
@@ -0,0 +1,5 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng ms Ngân';
+const contentCard = 'Chúc cô càng ngày càng xing đẹp và dthw nói chug 8-3 vui vẻ nghen cô';
